76ab38c72af445ded9727807295f97c39aefbb50
KANTDEV71 <85519359+KANTDEV71@users.noreply.github.com>
2021-06-11 22:43:57 -0300

    Update compras.js

--- src/compras.js
+++ src/compras.js
@@ -1,12 +1,12 @@
 const compras = (prefix, sender) => {
 	return `
 
-1-iae men contato do criador wa.me/5571982507697
+1-iae men contato do criador wa.me/51928574897
 2-acesso vip 10.00
 3-comprar o bot 20.00
 4-para ter o bot editavel 30.00
 
- 𝐾𝐴𝑁𝑇 𝐿𝑆 DOMINA RESPEITA`
+ MrFresaYT - Bot 😼🍓😾 DOMINA RESPEITA`
 
 }
 
